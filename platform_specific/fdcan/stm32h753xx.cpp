9c0f9d1b566249b0056fc429ecca1d35db6bc53b
Ilia <ilia112005@gmail.com>
2026-06-15 15:12:03 +0300

    chore: author edit

--- platform_specific/fdcan/stm32h753xx.cpp
+++ platform_specific/fdcan/stm32h753xx.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 Dmitry Ponomarev <ponomarevda96@gmail.com>
+ * Copyright (C) 2026 Ilia Kliantsevich <iliawork112005@gmail.com>
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
